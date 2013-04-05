5a8db4063508067ce11f653f0c15cf677ccae8b2
Raphael Crawford-Marks <raphael.crawfordmarks@gmail.com>
2013-04-05 15:53:25 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NewrelicMongoTestApp::Application.config.secret_token = '36815ab76e119924fcd8309dc81e36d45778131ac6890b43d5cd216f6668822b952d2fc67891f4b608b842adf77cd078accde86132e25f937027a99bd9f2ecd0'
